f0684cc1389739c4ec34590177f0728f510fbe20
Sabarigirish Manikandan <sabari.girish2057@gmail.com>
2025-01-01 13:35:20 +0530

    switched to fish. ditched kde for river

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+# set JAVA_HOME "/usr/lib/jvm/java-21-temurin/"
+# set ANDROID_HOME "$HOME/Android/cmdline-tools/"
+# set PATH "$HOME/.cargo/bin:$ANDROID_HOME/latest/bin:$HOME/Android/build-tools/36.0.0-rc3/:$HOME/Android/emulator/:$HOME/Android/platform-tools/:$PATH"
+if status is-interactive
+    starship init fish | source
+    alias l="eza --color=always --icons=always -a"
+    alias c=z
+    alias fzf="fzf --preview 'bat --style=numbers --color=always {}'"
+    alias lg=lazygit
+    alias cl=clear
+    alias lt="l --tree"
+    alias v=nvim
+    zoxide init fish | source
+end
